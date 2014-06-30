a9270dabebb3ab303c3f61e20a4bd4fb771498f9
David VanScott <david.vanscott@gmail.com>
2014-06-29 23:33:08 -0400

    Fix a route with the main menu

--- Nexus/Core/views/layouts/mainmenu.blade.php
+++ Nexus/Core/views/layouts/mainmenu.blade.php
@@ -1,5 +1,5 @@
 <ul>
     <li><a href={{ URL::route('home') }}>Home</a></li>
     <li><a href={{ URL::route('fleet') }}>Fleet Command</a></li>
-    <li><a href={{ URL::route('wiki') }}>LCARS Database</a></li>
+    <li><a href={{ URL::route('wiki.home') }}>LCARS Database</a></li>
 </ul>
