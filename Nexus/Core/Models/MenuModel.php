44efe179874a20298b4c0a41da467e5d02d6bb4a
David VanScott <david.vanscott@gmail.com>
2014-07-02 15:06:31 -0400

    Update the menu model
    Add a method to pull menu items so you can do one place and make any
    changes for everywhere

--- Nexus/Core/Models/MenuModel.php
+++ Nexus/Core/Models/MenuModel.php
@@ -1,13 +1,21 @@
 <?php namespace Nexus\Core\Models;
 
 use Illuminate\Database\Eloquent\SoftDeletingTrait,
-	Eloquent;
+	Model;
 
-class MenuModel extends Eloquent
+class MenuModel extends Model
 {
 	use SoftDeletingTrait;
 
 	protected $table = 'menu_sub' ;
 	protected $fillable = ['text', 'link', 'pg_class', 'sequence', 'active'] ;
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+	public static function getItems($pageClass)
+	{
+		return static::startQuery()->where('active', (int) true)
+			->where('pg_class', $pageClass)
+			->orderBy('sequence', 'asc')
+			->get();
+	}
 }
