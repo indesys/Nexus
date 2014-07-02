36ad82e6788b85eddbfd2cd0403bb698e0cde9fc
David VanScott <david.vanscott@gmail.com>
2014-07-02 16:57:52 -0400

    Department model fixes
    Fix ordering field and make sure positions are ordered properly when
    pulled through the relationship.

--- Nexus/Core/Models/DepartmentModel.php
+++ Nexus/Core/Models/DepartmentModel.php
@@ -8,12 +8,13 @@ class DepartmentModel extends Eloquent {
     use SoftDeletingTrait;
 
     protected $table = 'departments';
-    protected $fillable = ['name', 'dpt_order', 'description', 'image', 'url', 'admin'];
+    protected $fillable = ['name', 'sequence', 'description', 'image', 'url', 'admin'];
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 
     public function positions()
     {
-        return $this->hasMany('PositionModel', 'dept_id')->orderBy('pos_order');
+        return $this->hasMany('PositionModel', 'dept_id')
+            ->orderBy('sequence', 'asc');
     }
 
     public function scopeAdmin($query)
