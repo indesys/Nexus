d98177ec9c471db1fe7ff5de65b7405026a34c37
David VanScott <david.vanscott@gmail.com>
2014-07-02 15:06:37 -0400

    Add a base controller

---
+++ Nexus/Core/Controllers/BaseController.php
@@ -0,0 +1,15 @@
+<?php namespace Nexus\Core\Controllers;
+
+use Controller;
+
+abstract class BaseController extends Controller {
+
+    protected $pageClass;
+    protected $subnav;
+
+    public function __construct()
+    {
+        //
+    }
+
+}
