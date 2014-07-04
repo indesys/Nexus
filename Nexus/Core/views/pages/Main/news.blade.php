8c2a4d56e72a2860531bdc4ad6cf586dbb6632fc
David VanScott <david.vanscott@gmail.com>
2014-07-04 14:58:36 -0400

    Showing a test of the data table

--- Nexus/Core/views/pages/Main/news.blade.php
+++ Nexus/Core/views/pages/Main/news.blade.php
@@ -14,4 +14,19 @@
 		<p class="text-muted text-small">Posted under <strong>{{ $news->category->name }}</strong> on {{ $news->posted->format('F j, Y @ Hi') }} hours</p>
 	    <hr>
 	@endforeach
+
+	<div class="data-table data-table-striped data-table-bordered">
+	@foreach ($news_items as $news)
+		<div class="row">
+			<div class="col-md-6 col-lg-8">
+				<h3>{{ $news->title }}</h3>
+				<p class="text-muted">{{ nl2br($news->content) }}</p>
+			</div>
+			<div class="col-md-6 col-lg-4">
+				<p>{{ $news->category->name }}</p>
+				<p class="text-muted">{{ $news->posted->format('F j, Y @ Hi') }}</p>
+			</div>
+		</div>
+	@endforeach
+	</div>
 @stop
