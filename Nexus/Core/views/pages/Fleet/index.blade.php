99a28c34cdf1fefec163348f971636b32c4c5eeb
David VanScott <david.vanscott@gmail.com>
2014-07-05 13:10:52 -0400

    View cleanup

--- Nexus/Core/views/pages/Fleet/index.blade.php
+++ Nexus/Core/views/pages/Fleet/index.blade.php
@@ -1,17 +1,11 @@
 @extends('layouts.master')
 
 @section('title')
-
-Main Fleet Page
-
+    Fleet Command
 @stop
 
-
-
 @section('body')
+    <h1>The Fleet</h1>
 
-<h1>The Fleet</h1>
-
-<p>This is where the meat of the fleet is. Please use the links on the left to access various stats and information, and to search for positions.</p>
-
+    <p>This is where the meat of the fleet is. Please use the links on the left to access various stats and information, and to search for positions.</p>
 @stop
--- Nexus/Core/views/pages/Fleet/rules.blade.php
+++ Nexus/Core/views/pages/Fleet/rules.blade.php
@@ -1,19 +1,13 @@
 @extends('layouts.master')
 
 @section('title')
-
-Fleet Rules
-
+    Fleet Rules
 @stop
 
-
-
 @section('body')
+    <h1>Fleet Rules</h1>
 
-<h1>Fleet Rules</h1>
+    <p>Below are the rules that all players must abide by while operating in or as part of the fleet. Anyone found to be in breach of these rules, may be subject to disciplinary actions.</p>
 
-<p>Below are the rules that all players must abide by while operating in or as part of the fleet. Anyone found to be in breach of these rules, may be subject to disciplinary actions.</p>
-
-<p>{{ $rules->rules }}</p>
-
+    <p>{{ $rules->rules }}</p>
 @stop
